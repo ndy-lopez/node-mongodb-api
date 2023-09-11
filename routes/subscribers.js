f692ae2aeeb854e0471d46c0a74bf7f75dcffc6e
Andy Loredo <ndy.loredo@gmail.com>
2023-09-11 10:52:32 -0400

    Connect and test the Schema to the Read all route, empty array = no subscribers

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,33 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Subscriber = require("../models/subscriber");
 
 // Create a subscriber
-router.post('/', (req, res) => {
-  // Create a new subscriber and save it to the database (you need to add database code here)
-  res.send('Subscriber created');
+router.post("/", (req, res) => {
+  res.send("Subscriber created");
 });
 
 // Read all subscribers
-router.get('/', (req, res) => {
-  // Retrieve all subscribers from the database (you need to add database code here)
-  res.send('List of subscribers');
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Read one subscriber by ID
-router.get('/:id', (req, res) => {
-  // Retrieve a single subscriber by ID from the database (you need to add database code here)
+router.get("/:id", (req, res) => {
   res.send(`Subscriber with ID ${req.params.id}`);
 });
 
 // Update a subscriber by ID
-router.patch('/:id', (req, res) => {
-  // Update a subscriber by ID in the database (you need to add database code here)
+router.patch("/:id", (req, res) => {
   res.send(`Updated subscriber with ID ${req.params.id}`);
 });
 
 // Delete a subscriber by ID
-router.delete('/:id', (req, res) => {
-  // Delete a subscriber by ID from the database (you need to add database code here)
+router.delete("/:id", (req, res) => {
   res.send(`Deleted subscriber with ID ${req.params.id}`);
 });
 
